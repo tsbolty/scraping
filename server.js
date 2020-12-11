a2b1c80c2cf603a7dd6e9e9b48fa84aa42306d7c
Tyler Bolty <tsbolty@gmail.com>
2020-12-11 12:38:33 -0700

    updated output path

--- server.js
+++ server.js
@@ -1,9 +1,8 @@
-
 const path = require('path')
 const scrape = require('website-scraper');
 const options = {
   urls: ['https://www.rtd-denver.com/app/nextride/stop/34236'],
-  directory: path.join(__dirname + '/path/to/save1/')
+  directory: path.join(__dirname + '/output')
 };
 
-scrape(options).then((result) => console.log(result[0].Resource.children));
+scrape(options).then((result) => console.log(result));
